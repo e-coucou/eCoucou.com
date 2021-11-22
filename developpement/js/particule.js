499d3d72fe092a3a8d1d8b4bac3b4236ac42aa4d
e-Coucou <eric@e-coucou.com>
2021-11-22 06:50:21 +0100

    Colonne v0

--- developpement/js/particule.js
+++ developpement/js/particule.js
@@ -1,30 +1,78 @@
 class Particule {
-  constructor(pos_, d_, c_) {
-    this.pos = pos_;
-    this.vel = createVector(random(0.4, 0.6), 0);
+  constructor(pos_, t_) {
+    this.pos = pos_.copy();
     this.acc = createVector(0, 0);
-    this.density = d_;
-    this.color = c_;
+    this.up = false;
+    this.enter = false;
+    this.type = t_;
+    switch (this.type) {
+      case 'I':
+        this.sens = 1;
+        this.density = 12;
+        this.color = color(0, 255, 0, 155);
+        this.vel = createVector(0, -this.sens * 4); //random(0.4, 0.6), 0);
+        break;
+      case 'a': // legers
+        this.sens = -1;
+        this.density = 5;
+        this.color = color(0, 0, 255, 155);
+        this.vel = createVector(0, -this.sens * 3); //random(0.4, 0.6), 0);
+        break;
+      case 'b': // lourds
+        this.sens = 1;
+        this.density = 8;
+        this.color = color(255, 0, 0, 155);
+        this.vel = createVector(0, -this.sens * 2); //random(0.4, 0.6), 0);
+        break;
+    }
   }
 
   update() {
     this.vel.add(this.acc);
-    this.vel.limit(0.5);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
 
   applyForce(force) {
-    this.acc = force;
+    this.acc.add(force);
+  }
+
+  seek(cible) {
+    let desired = p5.Vector.sub(cible, this.pos).limit(1);
+    let steer = p5.Vector.sub(desired, this.vel);
+    return steer;
   }
 
   isColonne() {
-    return (this.pos.x < 350 && this.pos.x > 50);
+    let inC = (this.pos.x < 250 && this.pos.x > 150 && this.pos.y > 100 && this.pos.y < 500);
+    if (this.up) {
+      if (!inC) {
+        return false;
+      }
+    } else if (this.enter) {
+      if (this.pos.x > 230) {
+        this.up = true;
+      } else if (random() > 0.9) {
+        this.up = true;
+      }
+    } else if (inC) {
+      this.enter = true;
+    }
+    return this.up;
+  }
+
+  edges() {
+    return (this.pos.y <= 50 || this.pos.y >= height - 50);
+  }
+
+  paroies() {
+    if (this.pos.x < 100 || this.pos.x > 300) {
+      this.vel.x *= -2;
+    }
   }
 
   show() {
     stroke(this.color);
-    strokeWeight(8);
     strokeWeight(this.density);
     noFill();
     point(this.pos.x, this.pos.y);
--- developpement/js/sketch.js
+++ developpement/js/sketch.js
@@ -4,20 +4,42 @@ let particules = [];
 function setup() {
   canvas = createCanvas(400, 600);
   canvas.parent('sketch-dev');
-
+  particules.push(new Particule(createVector(width / 2, height / 2), 'I'));
+  background(0);
+  // frameRate(5);
 }
 
 function draw() {
   background(0);
-  if (random() > 0.95) {
-    particules.push(new Particule(createVector(0, height / 2), 8, color(0, 0, 255, 255)));
+  if (random() > 0.5) {
+    particules.push(new Particule(createVector(random(-100, 100) + width / 2, height / 3), 'I'));
   }
+  for (let i = particules.length - 1; i >= 0; i--) {
 
-  for (p of particules) {
-    if (p.isColonne()) {
-      p.applyForce(createVector(0, random(-0.02, -0.01)));
+    // if (p.isColonne()) {
+    let a = random() * PI;
+    // let V = createVector(width / 2, (p.sens + 1) * height / 2);
+    let dev = p5.Vector.fromAngle(particules[i].sens * a).setMag(0.1); //random(0.5, 0.7));
+    if (frameCount % 3 === 1) {
+      particules[i].vel.add(dev);
     }
-    p.update();
-    p.show();
+    if (particules[i].type === 'b' || particules[i].type === 'I') {
+      particules[i].vel.setMag(0.7);
+    } else if (particules[i].type === 'a') {
+      particules[i].vel.setMag(1);
+    }
+    particules[i].paroies();
+    particules[i].update();
+    particules[i].show();
+    if (particules[i].edges()) {
+      particules.splice(i, 1);
+    } else {
+      if (particules[i].type === 'I' && random() > 0.995) {
+        particules.push(new Particule(particules[i].pos, 'a'));
+        particules.push(new Particule(particules[i].pos, 'a'));
+        particules.push(new Particule(particules[i].pos, 'b'));
+        particules.splice(i, 1);
+      }
+    }
   }
 }
