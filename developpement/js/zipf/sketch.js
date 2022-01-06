3fba9ef90ce98799dbc5529d03c6bdec56eae02a
e-Coucou <eric@e-coucou.com>
2022-01-07 00:15:14 +0100

    yes

--- developpement/js/zipf/sketch.js
+++ developpement/js/zipf/sketch.js
@@ -2,7 +2,7 @@ let canvas;
 let grille;
 
 function setup() {
-    canvas = createCanvas(400, 400);
+    canvas = createCanvas(800, 400);
     canvas.parent("sketch-id");
     grille = new Grille(80, 80, 5);
 }
