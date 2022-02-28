558f701b82c93195e67c014a082a543bb9683deb
e-Coucou <eric@e-coucou.com>
2022-02-28 23:38:02 +0100

    umpdt

--- developpement/js/wordle/sketch.js
+++ developpement/js/wordle/sketch.js
@@ -1,6 +1,34 @@
 let canvas;
+let fichier;
+let liste_mots = []
+let mot;
+
+let col = 0;
+
+function preload() {
+    fichier = loadJSON("/developpement/js/wordle/mots_off.json")
+}
+
+function keyPressed() {
+    if (keyCode == BACKSPACE) {
+        col = 120;
+    } else if (keyCode == ENTER) {
+        col = 10;
+    } else if (key == 'A') {
+        col = 200;
+    }
+}
 
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.parent("sketch-id");
+    for (l in fichier) {
+        liste_mots.push(fichier[l]);
+    }
+    mot = random(liste_mots);
+    console.log(mot);
+}
+
+function draw() {
+    background(col)
 }
